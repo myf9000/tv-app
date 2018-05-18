0dd8785ae0a55627d8c313e11a38371b3cb15537
mkruszyk <kruszykm@gmail.com>
2018-05-18 21:31:23 +0200

    Added method to hash user password before saving.

--- db/models/user.js
+++ db/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 const Schema = mongoose.Schema;
 
@@ -68,5 +69,21 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth',
   });
 };
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (e, hash) => {
+        user.password = hash;
+
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 module.exports = mongoose.model('User', UserSchema);
